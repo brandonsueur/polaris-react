e612427b99a2caa0f4840aedaecc7e4d4879abe6
Andrea Herscovich <andrea.herscovich@shopify.com>
2020-06-28 17:12:08 -0400

    Added optional boolean notRetain prop

--- src/components/PositionedOverlay/PositionedOverlay.tsx
+++ src/components/PositionedOverlay/PositionedOverlay.tsx
@@ -36,6 +36,7 @@ export interface PositionedOverlayProps {
   preferredAlignment?: PreferredAlignment;
   fullWidth?: boolean;
   fixed?: boolean;
+  notRetain?: boolean;
   classNames?: string;
   render(overlayDetails: OverlayDetails): React.ReactNode;
   onScrollOut?(): void;
@@ -121,7 +122,7 @@ export class PositionedOverlay extends React.PureComponent<
 
   render() {
     const {left, right, top, zIndex, width} = this.state;
-    const {render, fixed, classNames: propClassNames} = this.props;
+    const {render, fixed, notRetain, classNames: propClassNames} = this.props;
 
     const style = {
       top: top == null || isNaN(top) ? undefined : top,
@@ -134,6 +135,7 @@ export class PositionedOverlay extends React.PureComponent<
     const className = classNames(
       styles.PositionedOverlay,
       fixed && styles.fixed,
+      notRetain && styles.notRetain,
       propClassNames,
     );
 
